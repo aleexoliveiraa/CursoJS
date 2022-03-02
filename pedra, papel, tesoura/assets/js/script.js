a0003b64e96e45101ac54ddefb868e93aad3b695
Alex Oliveira <96848961+aleexoliveiraa@users.noreply.github.com>
2022-03-02 10:03:45 -0300

    atualização do jogo

--- pedra, papel, tesoura/assets/js/script.js
+++ pedra, papel, tesoura/assets/js/script.js
@@ -10,27 +10,26 @@ function resposta (jogador, computador) {
 }
 
 function jogada(opcao) {
-    const opcoes = ['Pedra', 'Papel', 'Tesoura'];
-    const computador = Math.round(Math.random() * (2 - 0));
+    const opcoes = ['Pedra', 'Papel', 'Tesoura', 'Largato', 'Spock'];
+    const computador = Math.round(Math.random() * (4 - 0));
     console.log(computador)
-    if (opcao === 0 && computador === 0) {
-        resultado.innerHTML = `${opcoes[0]} são iguais ${opcoes[0]}. Empate!`;
-    } else if (opcao === 1 && computador === 0) {
+    if (opcao === computador) {
+        resultado.innerHTML = `${opcoes[opcao]} e ${opcoes[computador]} são iguais. Empate!`;
+    } else if (opcao === 1 && computador === 0 || computador === 1 && opcao === 0) {
         resultado.innerHTML = `${opcoes[1]} cobre ${opcoes[0]}. Você ganhou!`;
-        pontuacaoJogador.innerHTML = pontoJogador++;
+        if (opcao === 1) {
+            pontuacaoJogador.innerHTML = pontoJogador++;
+        }
+        
     } else if (opcao === 2 && computador === 0) {
         resultado.innerHTML = `${opcoes[0]} quebra ${opcoes[2]}. Você perdeu!`;
         pontuacaoComputador.innerHTML = pontoComputador++;
-    } else if (opcao === 1 && computador === 1) {
-        resultado.innerHTML = `${opcoes[0]} são iguais ${opcoes[0]}. Empate!`;
     } else if (opcao === 0 && computador === 1) {
         resultado.innerHTML = `${opcoes[1]} cobre ${opcoes[0]}. Você perdeu!`;
         pontuacaoComputador.innerHTML = pontoComputador++;
     } else if (opcao === 2 && computador === 1) {
         resultado.innerHTML = `${opcoes[2]} corta ${opcoes[1]}. Você ganhou!`;
         pontuacaoJogador.innerHTML = pontoJogador++;
-    } else if (opcao === 2 && computador === 2) {
-        resultado.innerHTML = `${opcoes[0]} são iguais ${opcoes[0]}. Empate!`;
     } else if (opcao === 0 && computador === 2) {
         resultado.innerHTML = `${opcoes[1]} cobre ${opcoes[0]}. Você perdeu!`;
         pontuacaoComputador.innerHTML = pontoComputador++;
