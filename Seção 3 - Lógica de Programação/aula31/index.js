db904efb3ac4d8e2a41dc275951ec3186d698bdf
Alex Oliveira <96848961+aleexoliveiraa@users.noreply.github.com>
2022-03-14 14:46:25 -0300

    aula 31

--- Seção 3 - Lógica de Programação/aula31/index.js
+++ Seção 3 - Lógica de Programação/aula31/index.js
@@ -1,16 +1,16 @@
 const verdadeira = true;
 
-let nome = 'Luiz';
-var nome2 = 'Luiz';
+let nome = 'Luiz'; //CRIANDO A VARIÁVEL
+var nome2 = 'Luiz'; //CRIANDO A VARIÁVEL
 console.log('Escopo Global', nome, nome2);
 
 if (verdadeira) {
-    let nome = 'Alex';
+    let nome = 'Alex'; //CRIANDO UMA NOVA VARIÁVEL
     var nome2 = 'André'; //REDECLARADA
     console.log('Escopo nivel 01', nome, nome2);
 
     if (verdadeira) {
-        let nome = 'Outra coisa';
+        let nome = 'Outra coisa'; //CRIANDO UMA NOVA VARIÁVEL
         var nome2 = 'Paulo'; //REDECLARADA
         console.log('Escopo nivel 02', nome, nome2);
     }
